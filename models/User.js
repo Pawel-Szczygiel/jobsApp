b889bd72056b5a28d00424f55cf9934d5eed027c
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2022-10-29 13:35:59 +0200

    starting project

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please provide name'],
+        minlength: 3,
+        maxlength: 20,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true,'Please provide email'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true,'Please provide password'],
+        minlength: 6,
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
+
