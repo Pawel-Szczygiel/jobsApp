14198a2073dfd1294a578143413b16f7839ad195
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2022-11-02 09:49:18 +0100

    add custom error handler

--- models/User.js
+++ models/User.js
@@ -14,12 +14,12 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true,'Please provide email'],
-        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email']
-        // unique: true
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true,'Please provide assword'],
+        required: [true,'Please provide password'],
         minlength: 6
     }
 });
